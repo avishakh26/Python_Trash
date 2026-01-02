d6b2f05d3a08a2a7d3de7d77ddcef0afb7c99bbf
Avishakh Chakrabortty <studiouspook@gmail.com>
2026-01-03 01:31:39 +0600

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,9 @@
+name= "Mr X"
+age = 16
+age2 = 18
+gpa= 4.99
+
+print(name+ " is known as a shit")
+print(name+" is a bad boy  ")
+print("he score ",gpa," in psc exam \t his age is ",age)
+print("when he enterd ",age2,"he was used to \n mix with bad boys")

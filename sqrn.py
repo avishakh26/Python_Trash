223dd0669a7d478f8df1ff86f955d7a6af37a45c
Avishakh Chakrabortty <studiouspook@gmail.com>
2026-01-10 21:31:59 +0600

    Add files via upload

---
+++ sqrn.py
@@ -0,0 +1,8 @@
+while True:
+    n = input("Please enter a number (0 t0 exit): ")
+    n=int(n)
+    
+    if n==0:
+        break
+    
+    print("Square of" ,n, "is",n*n)
---
+++ sqrn_positive.py
@@ -0,0 +1,14 @@
+while True:
+    n = input("Please enter a positive number (o to exit) : ")
+    n = int(n)
+    if n < 0:
+        print("We only allow positive number. Please try again. ")
+        
+        
+        continue
+    if n == 0:
+        break
+
+    print("Square of", n , "is",n*n)
+
+

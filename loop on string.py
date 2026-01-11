4358b07301d2d6f41c4d29edad93aaed7e495130
Avishakh Chakrabortty <studiouspook@gmail.com>
2026-01-12 01:07:59 +0600

    Add files via upload

---
+++ loop on string.py
@@ -0,0 +1,86 @@
+# >>> country[1]
+# 'a'
+# >>> country[2]
+# 'n'
+# >>> country[3]
+# 'g'
+# >>> country[4]
+# 'l'
+# >>> country[5]
+# 'a'
+# >>> country[6]
+# 'd'
+# >>> country[7]
+# 'e'
+# >>> country[8]
+# 's'
+# >>> country[9]
+# 'h'
+# >>> country [10] 
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# IndexError: string index out of range
+# >>>
+
+
+
+
+
+
+'''
+>>> for c in country:
+...     print(c)
+...
+B
+a
+n
+g
+l
+a
+d
+e
+s
+h
+>>>
+'''
+
+
+country = "Bangla" + "Desh"
+print(country)
+
+
+
+
+
+
+x = "50" + "5"
+print(x)
+
+
+
+
+
+# >>> country = "Bangladesh"
+# >>> country.find("Ban")
+# 0
+# >>> country.find("ang")
+# 1
+# >>> country.find("Bengla")
+# -1
+# >>> country.find("desh")
+# 6
+
+
+
+# >>> country = " North Korea"
+# >>> new_country = country.replace("North","South")
+# >>> print(new_country)
+#  South Korea
+# >>>    
+# >>>
+# >>> text = "this is avishakh.this is my first tread for you"
+# >>> new_text = text.replace("this","This")
+# >>> print(new_text)
+# This is avishakh.This is my first tread for you
+# >>>
+

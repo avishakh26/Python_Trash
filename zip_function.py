e7de36d0124851823dd13f96778e699049a008be
Avishakh Chakrabortty <studiouspook@gmail.com>
2026-01-24 22:47:58 +0600

    Add files via upload

---
+++ zip_function.py
@@ -0,0 +1,9 @@
+#it basically combine a two list 
+#it helps to make one list by converting two list
+
+roll = [101 ,102 ,103 ,104, 105]
+
+name = ["Avishakh", "Rakin", "Toha", "Goffar", "Ajgor"]
+
+
+print(list (zip(roll,name,"AAABA")))   #AAABA is their section. It'll print indevisually too

ef08ad446136978bb2b171a36e18b7085a130bac
Avishakh Chakrabortty <studiouspook@gmail.com>
2026-02-09 22:50:16 +0600

    Add files via upload

---
+++ deep_string.py
@@ -0,0 +1,35 @@
+''' 
+
+>>> text ="      this is a string    "    
+>>> text 
+'      this is a string    '
+
+
+>>> text.lstrip()
+'this is a string    '
+
+
+>>> text.rstrip()
+'      this is a string'
+
+
+>>> text.strip()
+'this is a string'
+>>>     
+
+'''
+
+# >>> s1 = "Bangladesh"
+
+# >>> new_s1 =s1.upper()    
+# >>> new_s1
+# 'BANGLADESH'
+
+# >>> s_lo = s1.lower()
+# >>> s_lo
+# 'bangladesh'
+
+# >>> s_cap = s1.capitalize()
+# >>> s_cap
+# 'Bangladesh'
+# >>>

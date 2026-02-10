abd28cee9a7682d635236df9329c28686e80d67d
Avishakh Chakrabortty <studiouspook@gmail.com>
2026-02-10 23:56:20 +0600

    Add files via upload

---
+++ work.py
@@ -0,0 +1,12 @@
+name = ["Likhon" , "Arafat" , "Meraj" , "Kotha" , "Avishakh"]
+id = [10 , 11 , 12 , 13 , 14]
+
+print(f"name : {name[0]} id : {id[0]}")
+
+print(f"name : {name[1]} id : {id[1]}")
+
+print(f"name : {name[2]} id : {id[2]}")
+
+print(f"name : {name[3]} id : {id[3]}")
+
+print(f"name : {name[4]} id : {id[4]}")
